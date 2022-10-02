59154e414634498b091fde067096567e934dfd74
hichem <hichem@hichem.com>
2022-10-02 19:40:53 +0100

    d

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" I love geometry! """
+
+
+class Square():
+    """ class square"""
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        """ initialise a Square class """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.height
+
+    def permiter_of_my_square(self):
+        """ Permiter of the square """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """ Reperense the square by defined string """
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+
+    a = square(width=12, height=9)
+    print(a)
+    print(a.area_of_my_square())
+    print(a.permiter_of_my_square())
